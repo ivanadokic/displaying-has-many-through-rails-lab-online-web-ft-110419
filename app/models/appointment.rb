24ffec360f5db0b78ea3bf44447a5938cbf3155c
Ivana <ivana_pile@yahoo.com>
2020-02-06 16:45:38 -0500

    Done.

---
+++ app/models/appointment.rb
@@ -0,0 +1,19 @@
+class Appointment < ActiveRecord::Base
+    belongs_to :doctor
+    belongs_to :patient
+
+    def doctor_name
+      self.doctor.name
+  end
+
+  def patient_name
+      self.patient.name
+  end
+  def convert_datetime_to_long_format
+    self.appointment_datetime.to_formatted_s(:long)
+  end
+
+  def convert_datetime_to_readable
+    self.convert_datetime_to_long_format.split.insert(3,"at").join(" ")
+  end  
+end
